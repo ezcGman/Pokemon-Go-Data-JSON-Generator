2321661d8d5a8988fad2dd6b7ab8510f31d86fcb
gman-php <ich@andy-hofmann.com>
2017-03-26 17:18:58 +0200

    Added types, badges, player levels, changed JSON layouts

--- run.py
+++ run.py
@@ -7,17 +7,33 @@ import re
 from v0_57_2_pb2 import *
 from google.protobuf.json_format import MessageToJson
 import csv
+from pprint import pprint
 
 
 # Read text CSVs
 pokemonTexts = {}
 with open('text-files/pokemon.txt') as csvfile:
-    reader = csv.DictReader(csvfile, dialect='excel-tab', fieldnames=['key', 'en', 'ja', 'fr', 'es', 'de', 'it', 'comment'])
+    reader = csv.DictReader(csvfile, dialect='excel-tab', fieldnames=['key', 'en', 'ja', 'fr', 'es', 'de', 'it', 'kr'])
     for row in reader:
         key = row['key']
         del row['key']
-        del row['comment']
         pokemonTexts[key] = row
+
+movesTexts = {}
+with open('text-files/moves.txt') as csvfile:
+    reader = csv.DictReader(csvfile, dialect='excel-tab', fieldnames=['key', 'en', 'ja', 'fr', 'es', 'de', 'it', 'kr'])
+    for row in reader:
+        key = row['key']
+        del row['key']
+        movesTexts[key] = row
+
+generalTexts = {}
+with open('text-files/general.txt') as csvfile:
+    reader = csv.DictReader(csvfile, dialect='excel-tab', fieldnames=['key', 'en', 'ja', 'fr', 'es', 'de', 'it', 'kr'])
+    for row in reader:
+        key = row['key']
+        del row['key']
+        generalTexts[key] = row
 
 # Read GAME_MASTER file
 with open('GAME_MASTER', mode='rb') as file: # b is important -> binary
@@ -30,7 +46,8 @@ decodedGameMaster.ParseFromString(fileContent)
 moves = {}
 pokemons = {}
 items = []
-types = []
+types = {}
+badges = {}
 for i in decodedGameMaster.items:
     if i.HasField('move'):
         moveId = i.move.unique_id
@@ -38,8 +55,14 @@ for i in decodedGameMaster.items:
         jsonObj = MessageToJson(i)
         move = json.loads(jsonObj)['move']
 
-        m = re.search('V[0-9]+_MOVE_(.*)', move['uniqueId'])
-        move['name'] = m.group(1).replace('_FAST', '').replace('_', ' ').lower().title()
+        moveNameKey = 'move_name_{:04d}'.format(moveId)
+        if moveNameKey in movesTexts:
+            move['name'] = movesTexts[moveNameKey]
+        else:
+            m = re.search('V[0-9]+_MOVE_(.*)', move['uniqueId'])
+            moveName = m.group(1).replace('_FAST', '').replace('_', ' ').lower().title()
+
+            move['name'] = {'en': moveName}
 
         move['moveId'] = moveId
 
@@ -51,6 +74,9 @@ for i in decodedGameMaster.items:
             i.pokemon.buddy_size = 0
 
         pokemonId = i.pokemon.unique_id
+        pokemonTypes = [i.pokemon.type1]
+        if i.pokemon.type2:
+            pokemonTypes.append(i.pokemon.type2)
 
         jsonObj = MessageToJson(i)
         pokemon = json.loads(jsonObj)['pokemon']
@@ -80,6 +106,11 @@ for i in decodedGameMaster.items:
 
         pokemon['pokemonId'] = pokemonId
 
+        pokemon['types'] = pokemonTypes
+        del pokemon['type1']
+        if i.pokemon.type2:
+            del pokemon['type2']
+
         pokemons[pokemonId] = pokemon
 
     elif i.HasField('item'):
@@ -87,8 +118,54 @@ for i in decodedGameMaster.items:
         items.append(jsonObj)
 
     elif i.HasField('type_effective'):
+        typeId = i.type_effective.attack_type
+
         jsonObj = MessageToJson(i)
-        types.append(jsonObj)
+        typeTmp = json.loads(jsonObj)
+        type = typeTmp['typeEffective']
+
+        m = re.search('POKEMON_TYPE_(.*)', typeTmp['templateId'])
+        typeName = m.group(1).replace('_', ' ').lower()
+
+        typeNameKey = 'pokemon_type_{:s}'.format(typeName)
+        if typeNameKey in generalTexts:
+            type['name'] = generalTexts[typeNameKey]
+        else:
+            type['name'] = {'en': typeName.title()}
+
+
+        type['typeId'] = typeId
+
+        types[typeId] = type
+
+    elif i.HasField('badge'):
+        badgeId = i.badge.badge_type
+
+        jsonObj = MessageToJson(i)
+        badgeTmp = json.loads(jsonObj)
+        badge = badgeTmp['badge']
+
+        m = re.search('BADGE_(.*)', badgeTmp['templateId'])
+        badgeName = m.group(1).replace('_', ' ').lower().title()
+        badge['name'] = {'en': badgeName}
+
+        badge['badgeId'] = badgeId
+
+        badges[badgeId] = badge
+
+    elif i.HasField('player_level'):
+        jsonObj = MessageToJson(i)
+        levelsTmp = json.loads(jsonObj)['playerLevel']
+
+        playerLevels = {}
+        idx = 1
+        for requiredExp in levelsTmp['requiredExp']:
+            playerLevels[idx] = {
+                'requiredExp': requiredExp,
+                'cpMultiplier': levelsTmp['cpMultiplier'][idx-1],
+                'rankNum': levelsTmp['rankNum'][idx-1]
+            }
+            idx += 1
 
 
 with open('out/pokemon.json', 'w') as outfile:
@@ -97,5 +174,9 @@ with open('out/moves.json', 'w') as outfile:
     json.dump(moves, outfile)
 with open('out/types.json', 'w') as outfile:
     json.dump(types, outfile)
+with open('out/badges.json', 'w') as outfile:
+    json.dump(badges, outfile)
 with open('out/items.json', 'w') as outfile:
     json.dump(items, outfile)
+with open('out/player-levels.json', 'w') as outfile:
+    json.dump(playerLevels, outfile)
